7a7b78d6422e242d6e5fbb221d1da76836baf530
IvanFitiskin <ifitiskin93@gmail.com>
2022-07-31 10:46:53 +0300

    added  unit tests for Feline class

---
+++ src/test/java/com/example/FelineTest.java
@@ -0,0 +1,55 @@
+package com.example;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(JUnitParamsRunner.class)
+public class FelineTest {
+
+    @Test
+    public void eatMeatIsPredatorFoods() throws Exception {
+        Feline feline = new Feline();
+
+        assertEquals(
+                List.of("Животные", "Птицы", "Рыба"),
+                feline.eatMeat()
+        );
+    }
+
+    @Test
+    public void getFamilyIsFelineString() {
+        Feline feline = new Feline();
+
+        assertEquals(
+                "Кошачьи",
+                feline.getFamily()
+        );
+    }
+
+    @Test
+    public void getKittensWithoutArgumentOneResult() {
+        Feline feline = new Feline();
+
+        assertEquals( 1, feline.getKittens());
+    }
+
+    @Test
+    @Parameters({
+            "0, 0",
+            "1, 1",
+            "5, 5"
+    })
+    public void getKittensWithPositiveArgumentSameResult(int argumentValue, int expectedValue) {
+        Feline feline = new Feline();
+
+        assertEquals( expectedValue, feline.getKittens(argumentValue));
+    }
+
+}
