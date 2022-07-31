88a96e7dbf4933cb2d0eff0dd39b728c0d8b04f8
IvanFitiskin <ifitiskin93@gmail.com>
2022-07-31 11:35:40 +0300

    added tests for Animal class

---
+++ src/test/java/com/example/AnimalTest.java
@@ -0,0 +1,60 @@
+package com.example;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnitParamsRunner.class)
+public class AnimalTest {
+
+    @Test
+    public void getFamilyIsStringStaticValue() {
+        Animal animal = new Animal();
+
+        assertEquals(
+                "Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи",
+                animal.getFamily()
+        );
+    }
+
+    @Test
+    @Parameters(method = "parametersToGetFoodTest")
+    public void getFoodStringCorrectValueListOfFoods(
+            String argumentValue,
+            List<String> expectedValue
+    ) throws Exception {
+        Animal animal = new Animal();
+
+        assertEquals(expectedValue, animal.getFood(argumentValue));
+    }
+
+    @Test(expected = Exception.class)
+    @Parameters(method = "parametersToGetFoodThrowsExceptionTest")
+    public void getFoodThrowsExceptionWhenInitialisedWithWrongAnimalKind(
+            String expected
+    ) throws Exception {
+        Animal animal = new Animal();
+        animal.getFood(expected);
+    }
+
+    private Object[] parametersToGetFoodTest() {
+        return new Object[] {
+                new Object[] { "Травоядное", List.of("Трава", "Различные растения") },
+                new Object[] { "Хищник", List.of("Животные", "Птицы", "Рыба") },
+        };
+    }
+
+    private Object[] parametersToGetFoodThrowsExceptionTest() {
+        return new Object[] {
+                // Первая буква в слове "Травоядноe" указана латиницей
+                new Object[] { "Травоядноe" },
+                new Object[] { "Всеядный" },
+        };
+    }
+
+}
